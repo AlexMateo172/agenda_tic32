f4d6fae6e34f621a4040ee1e828f37c9303baa1f
AlexMateo172 <1725110135@utectulancingo.edu.mx>
2026-06-23 15:53:51 +0000

    CREATED .agenda

---
+++ agenda/app.py
@@ -0,0 +1,37 @@
+import web
+
+
+urls = (
+    '/', 'index',
+    '/lista_contactos','Lista_contactos',
+    '/ver_contacto','Ver_contacto',
+    '/editar_contacto','Editar_contacto',
+    '/borrar_contacto','Borrar_contacto'
+    
+)
+app = web.application(urls, globals())
+render = web.template.render('views')
+
+class index:
+    def GET(self):
+        return render.index()
+    
+class Lista_contactos:
+    def GET(self):    
+        return render.lista_contactos()
+
+class Ver_contacto:
+     def GET(self):
+         return render.ver_contacto()
+
+class Editar_contacto:
+     def GET(self):
+         return render.editar_contacto()   
+
+class Borrar_contacto:
+     def GET(self):
+         return render.borrar_contacto()          
+
+
+if __name__ == "__main__":
+    app.run()
